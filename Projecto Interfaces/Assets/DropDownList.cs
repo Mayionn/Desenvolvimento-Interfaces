f642791f6edcb39b752c9ea1af89c5b98f8cb30a
Débora FM <debofm@gmail.com>
2018-12-14 00:35:17 +0000

    more pages fuck dropdowns

---
+++ Projecto Interfaces/Assets/DropDownList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDownList : MonoBehaviour {
+
+    public Dropdown continents;
+    public Dropdown regions;
+
+
+    public Dropdown m_Dropdown;
+    int m_DropdownValue;
+
+    void Start()
+    {
+        m_Dropdown = GetComponent<Dropdown>();
+        Debug.Log("Starting Dropdown Value : " + m_Dropdown.value);
+    }
+
+    void Update () {
+
+        m_DropdownValue = m_Dropdown.value;
+        if (m_DropdownValue == 1)
+        {
+            continents.enabled = true;
+
+        }
+        if (m_DropdownValue == 2)
+        {
+            regions.enabled = true;
+
+        }
+
+    }
+}
